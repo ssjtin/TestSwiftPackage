5f9c552daaebe48efa33895e0a78dd8071807766
Hoang Luong <ssjtin28@hotmail.com>
2021-01-30 19:44:51 +1000

    Morework

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TestPackage",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13)
+        .iOS(.v13)
     ],
     products: [
         .library(
--- Sources/TestPackage/Files/StringsForHire.swift
+++ Sources/TestPackage/Files/StringsForHire.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+#if !os(macOS)
+import UIKit
 
 public struct StringsForHire {
     public static let instrument = "Violin 4/4"
+    public func image() -> UIImage? {
+        UIImage(named: "bernie_meme.png", in: Bundle.module, compatibleWith: nil)
+    }
 }
+
+#endif
