fc6ce379776f724bf1771b40fd47d9138d80d9b3
Hoang Luong <ssjtin28@hotmail.com>
2021-01-30 20:10:02 +1000

    File manager

--- Sources/TestPackage/Files/StringsForHire.swift
+++ Sources/TestPackage/Files/StringsForHire.swift
@@ -17,3 +17,23 @@ public struct StringsForHire {
 }
 
 #endif
+
+extension FileManager {
+    func copyfileToUserDocumentDirectory(forResource name: String,
+                                         ofType ext: String) throws
+    {
+        if let bundlePath = Bundle.main.path(forResource: name, ofType: ext),
+            let destPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                .userDomainMask,
+                                true).first {
+            let fileName = "\(name).\(ext)"
+            let fullDestPath = URL(fileURLWithPath: destPath)
+                                   .appendingPathComponent(fileName)
+            let fullDestPathString = fullDestPath.path
+
+            if !self.fileExists(atPath: fullDestPathString) {
+                try self.copyItem(atPath: bundlePath, toPath: fullDestPathString)
+            }
+        }
+    }
+}
